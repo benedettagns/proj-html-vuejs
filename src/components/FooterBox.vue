55b75412ccd335c6f213f6c13a7eeb538420637f
benedettagns <benedetta.genise01@gmail.com>
2022-02-10 22:10:53 +0100

    footer

--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -10,9 +10,37 @@
             </div>
         </div>
 
-        <div>
-            
+
+        <div class="information">
+            <div class="info-box">
+                <h4>COME FIND US</h4>
+                <p>12345 North Main Street</p>
+                <p>New York, NY 555555</p>
+                <p>Phone: 1.800.555.6789</p>
+                <p>Email: info@your-domain.com</p>
+            </div>
+
+            <div class="info-box">
+                <h4>RECENT POSTS</h4>
+                <p>> Train with free weights or your body weight?</p>
+                <hr>
+                <p>> Nutritional advice that will keep you training</p>
+                <hr>
+            </div>
+
+            <div class="info-box">
+                <h4>OPENING TIMES</h4>
+                <p>Weekdays Moday - Friday</p>
+                <p>09:00 - 19:00</p>
+                <p>Weekends Saturday - Sunday</p>
+                <p>09:00 - 21:00</p>
+            </div>
+
+            <div class="info-img">
+                <img src="../assets/gym_ad-compressor-200x133.jpg" alt="">
+            </div>
         </div>
+
     </footer>
 </template>
 
